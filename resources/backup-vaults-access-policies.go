db154520d5127bfacfec56a6f5043ba546e16bce
Singh, Suraj <suraj_singh940@optum.com>
2024-10-15 20:54:30 +0530

    Update backup-vaults-access-policies.go

--- resources/backup-vaults-access-policies.go
+++ resources/backup-vaults-access-policies.go
@@ -1,6 +1,7 @@
 package resources
 
 import (
+	"fmt"
 	"strings"
 
 	"github.com/aws/aws-sdk-go/aws/session"
@@ -122,7 +123,7 @@ func (b *BackupVaultAccessPolicy) String() string {
 
 func (b *BackupVaultAccessPolicy) Filter() error {
 	if strings.HasPrefix(b.backupVaultName, "aws/efs/automatic-backup-vault") {
-		return nil
+		return fmt.Errorf("cannot delete EFS automatic backups backup policy")
 	}
 	return nil
 }
