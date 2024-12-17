9c40863bfede8bd8bc4e1cb06a7fc3db9a377268
vtulaban_uhg <venkata_tulabandala@optum.com>
2024-12-17 09:32:02 -0600

    import error

--- resources/wafv2-webacls.go
+++ resources/wafv2-webacls.go
@@ -1,10 +1,9 @@
 package resources
 
 import (
-	"context"
-
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
+	cloudfront "github.com/aws/aws-sdk-go/service/cloudfront"
 	"github.com/aws/aws-sdk-go/service/wafv2"
 	"github.com/go-logr/logr"
 	"github.com/rebuy-de/aws-nuke/v2/pkg/types"
@@ -17,6 +16,7 @@ type WAFv2WebACL struct {
 	lockToken *string
 	scope     *string
 	logger    logr.Logger
+	//resourceARN *string
 }
 
 func init() {
@@ -69,6 +69,7 @@ func getWebACLs(svc *wafv2.WAFV2, params *wafv2.ListWebACLsInput) ([]Resource, e
 				name:      webACL.Name,
 				lockToken: webACL.LockToken,
 				scope:     params.Scope,
+				//resourceARN: webACL.ARN,
 			})
 		}
 
@@ -81,32 +82,85 @@ func getWebACLs(svc *wafv2.WAFV2, params *wafv2.ListWebACLsInput) ([]Resource, e
 	return resources, nil
 }
 
-// Disassociate WebACL which causing the failure to nuke WAFV2 objects
-func (f *WAFv2WebACL) DisassociateWebACL(ctx context.Context, resourceARN string) error {
-	req := &wafv2.DisassociateWebACLInput{
-		ResourceArn: aws.String(resourceARN),
+// List cloudfront distributions associated with the cloudfront   WebACL
+func (f *cloudfront) ListAssociatedCloudfrontDistributions() ([]*string, error) {
+	//svc := cloudfront.New(f.svc.Config)
+	params := &cloudfront.ListDistributionsInput{}
+
+	var distributions []*string
+	for {
+
+		resp, err := f.svc.ListDistributions(params)
+
+		if err != nil {
+			return nil, err
+		}
+
+		for _, distribution := range resp.DistributionList.Items {
+			if *distribution.WebACLId == *f.ID {
+				distributions = append(distributions, distribution.Id)
+			}
+		}
+
+		if resp.DistributionList.NextMarker == nil {
+			break
+		}
+
+		params.Marker = resp.DistributionList.NextMarker
 	}
-	f.logger.Info("disassociating WAFv2 webACL",
-		"resourceARN", resourceARN)
-	if _, err := f.svc.DisassociateWebACLWithContext(ctx, req); err != nil {
+	return distributions, nil
+}
+func (f *WAFv2WebACL) RemoveAssociatedCloudfrontDistributions() error {
+	distributions, err := f.ListAssociatedCloudfrontDistributions()
+	if err != nil {
 		return err
 	}
-	f.logger.Info("disassociated WAFv2 webACL", "resourceARN", resourceARN)
+
+	svc := cloudfront.New(f.svc.Config)
+	for _, distribution := range distributions {
+		_, err := svc.UpdateDistribution(&cloudfront.UpdateDistributionInput{
+			Id: distribution,
+			DistributionConfig: &cloudfront.DistributionConfig{
+				WebACLId: nil,
+			},
+			IfMatch: nil,
+		})
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func (f *WAFv2WebACL) RemoveAssociatedResources() error {
+	err := f.RemoveAssociatedCloudfrontDistributions()
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func UpdateDistribution(svc *cloudfront.CloudFront, distributionID *string, webACLID *string) error {
+	_, err := svc.UpdateDistribution(&cloudfront.UpdateDistributionInput{
+		Id: distributionID,
+		DistributionConfig: &cloudfront.DistributionConfig{
+			WebACLId: webACLID,
+		},
+		IfMatch: nil,
+	})
+	if err != nil {
+		return err
+	}
 	return nil
 }
 
 func (f *WAFv2WebACL) Remove() error {
 
-	err := f.DisassociateWebACL(ctx, "resourceARN")
-	if err != nil {
-		return err
-	}
-	
 	_, err := f.svc.DeleteWebACL(&wafv2.DeleteWebACLInput{
 		Id:        f.ID,
+		LockToken: f.lockToken,
 		Name:      f.name,
 		Scope:     f.scope,
-		LockToken: f.lockToken,
 	})
 	if err != nil {
 		return err
